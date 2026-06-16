3ad67a211425769fbfdd43a007e41dda88f812e1
Denny Mathew <denny.mathew@aviv-group.com>
2026-06-16 20:52:56 +0200

    initial commit

---
+++ TestRepo/TestRepo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestRepo
+//
+//  Created by Denny Mathew  on 16.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestRepo/TestRepo/TestRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoApp.swift
+//  TestRepo
+//
+//  Created by Denny Mathew  on 16.06.26.
+//
+
+import SwiftUI
+
+@main
+struct TestRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
